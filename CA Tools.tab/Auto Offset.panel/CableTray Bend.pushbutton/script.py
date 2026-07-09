4c49da707567e45cc48c6f1a39001eb477df9d2b
CA Tools_By Chulan Adasuriya <catools2026@gmail.com>
2026-07-09 09:28:48 +0530

    Add files via upload

---
+++ CA Tools.tab/Auto Offset.panel/CableTray Bend.pushbutton/script.py
@@ -0,0 +1,6 @@
+# GreaterBIM - CableTray Bend
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'lib'))
+from pyrevit import revit
+import bend_core
+bend_core.run_bend(revit.uidoc, "CableTray")
